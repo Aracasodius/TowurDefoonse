9a1739da3e1c380ba05e647128bdfad3ece63984
Davidson <grootloops@blueparrtogames.com>
2018-01-22 11:22:36 +0100

    New Tower Defense Project

---
+++ Assets/Dev stuff/Scripts/Buildable.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Buildable : MonoBehaviour
+{
+    private enum Object { Tower }
+
+    private Object obj;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        switch (obj)
+        {
+            case Object.Tower:
+                {
+                    break;
+                }
+        }
+    }
+}
