4adc4ab0c343ed812852a9cc6c793ceea045ddfd
Davidson <grootloops@blueparrtogames.com>
2018-01-23 14:59:49 +0100

    Big Push and world edit

--- Assets/Dev stuff/Scripts/Buildable.cs
+++ Assets/Dev stuff/Scripts/Buildable.cs
@@ -4,26 +4,34 @@ public class Buildable : MonoBehaviour
 {
 
     private Camera cam;
+    private bool CanvasOpen;
 
     private void Start()
     {
         cam = Camera.main;
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if ( Input.GetMouseButtonDown(1))
+        if ( Input.GetMouseButtonUp(1))
         {
-            RaycastHit hit;
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Tower" )
-            {
-                GameObject canv = (GameObject)Instantiate(Resources.Load("TowerSelect"));
-                canv.GetComponent<Canvas>().worldCamera = cam;
-                canv.GetComponent<RectTransform>().localPosition = hit.transform.position + new Vector3(0, 1.5f, 0.1f);
-                canv.transform.LookAt(cam.transform);
-                canv.GetComponentInChildren<BuildManager>().Socket = hit.transform.gameObject;
-            }
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit) && hit.transform.tag == "Tower" && !CanvasOpen)
+        {
+            GameObject canv = (GameObject)Instantiate(Resources.Load("TowerSelect"));
+            canv.GetComponent<Canvas>().worldCamera = cam;
+            canv.GetComponent<RectTransform>().localPosition = hit.transform.position + new Vector3(0, 1.5f, 0.1f);
+            canv.transform.LookAt(cam.transform);
+            canv.GetComponentInChildren<BuildManager>().Socket = hit.transform.gameObject;
+            CanvasOpen = true;
         }
     }
 }
